89a6d7527aa0eef5c70ed7d6bc9df967011e00cb
raghucrm <raghu@crmexpertsny.com>
2026-07-14 15:57:57 +0530

    Update suitecloud.config.js

--- suitecloud.config.js
+++ suitecloud.config.js
@@ -1,15 +1,4 @@
-const SuiteCloudJestUnitTestRunner = require('@oracle/suitecloud-unit-testing/services/SuiteCloudJestUnitTestRunner');
-
+// suitecloud.config.js
 module.exports = {
-	defaultProjectFolder: 'src',
-	commands: {
-		"project:deploy": {
-			beforeExecuting: async args => {
-				await SuiteCloudJestUnitTestRunner.run({
-				    // Jest configuration options.
-				});
-				return args;
-			},
-		},
-	},
+  defaultProjectFolder: "src",
 };
